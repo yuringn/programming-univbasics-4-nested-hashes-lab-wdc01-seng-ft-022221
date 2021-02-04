501d1be70fb8c71ab4ba06abbb24538be6e6948d
Yuri Ng <yuringuyen312@gmail.com>
2021-02-04 21:53:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -110,7 +110,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]= "GUI"
+p programmer_hash
 end
 
 def adding_to_dennis
@@ -132,5 +133,5 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages]<<"Assembly"
 end
