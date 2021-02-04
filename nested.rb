a2b19ca80da8a06c9cc10f4b838d2b44e37da180
Yuri Ng <yuringuyen312@gmail.com>
2021-02-04 21:41:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,9 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash[:yukihiro_matsumoto]={[:known_for => "Ruby",
+  :languages => ["LISP", "C"]
+ ]}
 end
 
 def changing_alan
